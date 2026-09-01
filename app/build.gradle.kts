41be3848eaf857905a42ee8eeb229d1de2ad3675
LuqmanKml1961 <luqmankamal1961@gmail.com>
2026-09-01 17:31:59 +0800

    Fix mockito-kotlin version: 3.2.1 does not exist on Maven Central, use 4.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation("org.mockito:mockito-core:5.11.0")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test:core:1.5.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
